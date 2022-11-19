2466b852cff4cac9ff775d9c5a6ee6ddda1b282e
John Dugan <john@john-dugan.com>
2022-11-19 14:10:36 -0600

    change rule severities from integer to string value and avoidEscape in quotes rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,101 +19,101 @@ module.exports = {
 
     // http://eslint.org/docs/rules/
     rules: {
-        'arrow-body-style': [1, 'as-needed'], // es6
-        'arrow-parens': [2, 'always'], // es6
-        'arrow-spacing': [1, {before: true, after: true}], // es6
-        'block-scoped-var': 2,
-        'brace-style': [1, '1tbs', {allowSingleLine: true}],
-        'comma-dangle': [1, 'never'],
-        'comma-spacing': [1, {before: false, after: true}],
-        'consistent-return': 1,
-        'constructor-super': 2, // es6
-        'curly': [2, 'all'],
-        'default-case': 1,
-        'dot-notation': [1, {allowKeywords: true}],
-        'eol-last': 1,
-        'eqeqeq': 1,
-        'indent': [1, 4, {SwitchCase: 1}],
-        'key-spacing': [1, {beforeColon: false, afterColon: true}],
-        'keyword-spacing': [1, {before: true, after: true}],
-        'linebreak-style': [1, 'unix'],
-        'new-cap': [2, {
+        'arrow-body-style': ['warn', 'as-needed'], // es6
+        'arrow-parens': ['error', 'always'], // es6
+        'arrow-spacing': ['warn', {before: true, after: true}], // es6
+        'block-scoped-var': 'error',
+        'brace-style': ['warn', '1tbs', {allowSingleLine: true}],
+        'comma-dangle': ['warn', 'never'],
+        'comma-spacing': ['warn', {before: false, after: true}],
+        'consistent-return': 'warn',
+        'constructor-super': 'error', // es6
+        'curly': ['error', 'all'],
+        'default-case': 'warn',
+        'dot-notation': ['warn', {allowKeywords: true}],
+        'eol-last': 'warn',
+        'eqeqeq': 'warn',
+        'indent': ['warn', 4, {SwitchCase: 'warn'}],
+        'key-spacing': ['warn', {beforeColon: false, afterColon: true}],
+        'keyword-spacing': ['warn', {before: true, after: true}],
+        'linebreak-style': ['warn', 'unix'],
+        'new-cap': ['error', {
             newIsCap: true,
             capIsNew: true,
             newIsCapExceptions: [],
             capIsNewExceptions: []
         }],
-        'new-parens': 2,
-        'no-case-declarations': 2,
-        'no-class-assign': 2, // es6
-        'no-confusing-arrow': [2, {allowParens: true}], // es6
-        'no-console': 1,
-        'no-const-assign': 2, // es6
-        'no-constant-condition': 2,
-        'no-div-regex': 2,
-        'no-dupe-class-members': 2, // es6
-        'no-else-return': 2,
-        'no-empty-pattern': 2,
-        'no-extend-native': 2,
-        'no-extra-bind': 2,
-        'no-extra-parens': [1, 'all'],
-        'no-fallthrough': 1,
-        'no-iterator': 2,
-        'no-lone-blocks': 2,
-        'no-loop-func': 1,
-        'no-multi-spaces': 1, // flags variable `=` alignment
-        'no-multi-str': 1,
-        'no-native-reassign': 2,
-        'no-new-func': 2,
-        'no-new-wrappers': 2,
-        'no-octal-escape': 2,
-        // "no-param-reassign": 2, // flags `myNum += 1`
-        'no-proto': 2,
-        'no-return-assign': 2,
-        'no-script-url': 2,
-        'no-self-assign': 1,
-        'no-self-compare': 1,
-        'no-sequences': 2,
-        'no-shadow': [2, {
+        'new-parens': 'error',
+        'no-case-declarations': 'error',
+        'no-class-assign': 'error', // es6
+        'no-confusing-arrow': ['error', {allowParens: true}], // es6
+        'no-console': 'warn',
+        'no-const-assign': 'error', // es6
+        'no-constant-condition': 'error',
+        'no-div-regex': 'error',
+        'no-dupe-class-members': 'error', // es6
+        'no-else-return': 'error',
+        'no-empty-pattern': 'error',
+        'no-extend-native': 'error',
+        'no-extra-bind': 'error',
+        'no-extra-parens': ['warn', 'all'],
+        'no-fallthrough': 'warn',
+        'no-iterator': 'error',
+        'no-lone-blocks': 'error',
+        'no-loop-func': 'warn',
+        'no-multi-spaces': 'warn', // flags variable `=` alignment
+        'no-multi-str': 'warn',
+        'no-native-reassign': 'error',
+        'no-new-func': 'error',
+        'no-new-wrappers': 'error',
+        'no-octal-escape': 'error',
+        // "no-param-reassign": 'error', // flags `myNum += 1`
+        'no-proto': 'error',
+        'no-return-assign': 'error',
+        'no-script-url': 'error',
+        'no-self-assign': 'warn',
+        'no-self-compare': 'warn',
+        'no-sequences': 'error',
+        'no-shadow': ['error', {
             builtinGlobals: true,
             hoist: 'all',
             allow: ['$', 'Plugin', 'self']
         }],
-        'no-shadow-restricted-names': 2,
-        'no-spaced-func': 2,
-        'no-this-before-super': 1, // es6
-        'no-throw-literal': 2,
-        'no-trailing-spaces': 1,
-        'no-undef': 2,
-        'no-undef-init': 2,
-        'no-undefined': 2,
-        'no-unneeded-ternary': 1,
-        'no-unexpected-multiline': 2,
-        'no-unused-expressions': [2, {allowShortCircuit: false, allowTernary: true}],
-        'no-unused-vars': [2, {vars: 'all', args: 'after-used'}],
-        'no-use-before-define': [2, 'nofunc'], // exception for function declarations
-        'no-useless-call': 1,
-        'no-useless-concat': 1,
-        'no-void': 2,
-        'no-with': 2,
-        'object-curly-spacing': [1, 'never'],
-        'object-shorthand': [1, 'never'], // es6
-        'prefer-arrow-callback': 1, // es6
-        'prefer-spread': 1, // es6
-        'prefer-template': 1, // es6
-        'quotes': [1, 'single'],
-        'radix': [2, 'as-needed'],
-        'semi': [2, 'always'],
-        'semi-spacing': [1, {before: false, after: true}],
-        'space-before-blocks': [1, {functions: 'always', keywords: 'always'}],
-        'space-before-function-paren': [1, {anonymous: 'never', named: 'never', asyncArrow: 'always'}], // match Prettier convention
-        'space-in-parens': [1, 'never'],
-        // "space-infix-ops": 1, // flags `'str'+'ing'`
-        'space-unary-ops': [1, {words: true, nonwords: false}],
-        'spaced-comment': [1, 'always', {exceptions: ['!']}], // exception for `!`
-        'vars-on-top': 1,
-        'wrap-iife': [1, 'outside'],
-        'wrap-regex': 1,
-        'yoda': [1, 'never']
+        'no-shadow-restricted-names': 'error',
+        'no-spaced-func': 'error',
+        'no-this-before-super': 'warn', // es6
+        'no-throw-literal': 'error',
+        'no-trailing-spaces': 'warn',
+        'no-undef': 'error',
+        'no-undef-init': 'error',
+        'no-undefined': 'error',
+        'no-unneeded-ternary': 'warn',
+        'no-unexpected-multiline': 'error',
+        'no-unused-expressions': ['error', {allowShortCircuit: false, allowTernary: true}],
+        'no-unused-vars': ['error', {vars: 'all', args: 'after-used'}],
+        'no-use-before-define': ['error', 'nofunc'], // exception for function declarations
+        'no-useless-call': 'warn',
+        'no-useless-concat': 'warn',
+        'no-void': 'error',
+        'no-with': 'error',
+        'object-curly-spacing': ['warn', 'never'],
+        'object-shorthand': ['warn', 'never'], // es6
+        'prefer-arrow-callback': 'warn', // es6
+        'prefer-spread': 'warn', // es6
+        'prefer-template': 'warn', // es6
+        'quotes': ['warn', 'single', {'avoidEscape': true}],
+        'radix': ['error', 'as-needed'],
+        'semi': ['error', 'always'],
+        'semi-spacing': ['warn', {before: false, after: true}],
+        'space-before-blocks': ['warn', {functions: 'always', keywords: 'always'}],
+        'space-before-function-paren': ['warn', {anonymous: 'never', named: 'never', asyncArrow: 'always'}], // match Prettier convention
+        'space-in-parens': ['warn', 'never'],
+        // "space-infix-ops": 'warn', // flags `'str'+'ing'`
+        'space-unary-ops': ['warn', {words: true, nonwords: false}],
+        'spaced-comment': ['warn', 'always', {exceptions: ['!']}], // exception for `!`
+        'vars-on-top': 'warn',
+        'wrap-iife': ['warn', 'outside'],
+        'wrap-regex': 'warn',
+        'yoda': ['warn', 'never']
     }
 };
